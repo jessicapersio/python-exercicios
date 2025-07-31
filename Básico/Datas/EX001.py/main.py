82804d520b546e627222b90b419f8ae33d78abbb
Jéssica Persio Fernanddes <jepersio@hotmail.com>
2025-07-31 18:17:54 +0000

    Orgaização das pastas e finalização do exercicio

--- Básico/Datas/EX001.py/main.py
+++ Básico/Datas/EX001.py/main.py
@@ -1,8 +1,8 @@
-#Pegar hora atual 
+#Carregue a data atual do computador e apresente somente a data
 import datetime 
 data_atual = datetime.datetime.now()
 print(data_atual.strftime("%x"))
-#Com a data apresentar a data que será daqui a 2 dias 
+#Carregue a data atual do computador e, com base na data atual, apresente a data de dois dias no futuro
 data_2_dias = datetime.timedelta(days=2) 
 data_futura = data_atual + data_2_dias 
 print(data_futura.strftime("%x"))
