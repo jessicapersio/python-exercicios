f300ef35c6cadc5db89875424a019294fdb98cc3
Jéssica Persio Fernanddes <jepersio@hotmail.com>
2025-08-01 13:36:59 +0000

    resolvendo exercicio de dicionario

---
+++ Básico/Dicionários/main.py
@@ -0,0 +1,6 @@
+"""
+Considere o seguinte dicionário: {'m1': {'m2': 'Olá Mundo'}}. 
+Carregue e apresente a mensagem "Olá Mundo" contida no dicionário
+"""
+ola = {"m1": {"m2": 'Olá mundo'}}
+print(ola["m1"]["m2"])
