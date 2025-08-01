d147f399b7291dd81a991a7e2b6c86ab9d694d48
Jéssica Persio Fernanddes <jepersio@hotmail.com>
2025-08-01 18:51:45 +0000

    Dois exericicos trazendo mensagens

---
+++ Básico/Mensagens/Ex002/main.py
@@ -0,0 +1,3 @@
+#Desenvolva um programa que pergunte o seu nome e, ao teclar Enter, apresente uma saudação personalizada
+x = input('Digite seu nome: ')
+print(f'Saudações, {x}! Seja bem-vindo(a)!')
