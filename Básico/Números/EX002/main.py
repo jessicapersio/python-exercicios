4710f8aef7e65be048f998992137161b2d6a865e
Jéssica Persio Fernanddes <jepersio@hotmail.com>
2025-08-07 12:16:48 +0000

    Resolvendo ex002 de números

---
+++ Básico/Números/EX002/main.py
@@ -0,0 +1,6 @@
+"""
+Desenvolva um programa que leia um número inteiro qualquer
+ e que apresente o número informado com duas casas decimais
+"""
+numero = float(input("Digite um número: "))
+print(f"Seu número com duas casas decimais é: {numero:.2f}")
