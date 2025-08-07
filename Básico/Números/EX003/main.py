8901c07c0384ff2d00b354e4304f3c30b778cf58
Jéssica Persio Fernanddes <jepersio@hotmail.com>
2025-08-07 12:28:46 +0000

    Resolução de exercicios de números em Python.

--- Básico/Números/EX003/main.py
+++ Básico/Números/EX003/main.py
@@ -6,4 +6,4 @@ nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota: "))
 nota4 = float(input("Digite a quarta nota: "))
 media = (nota1 + nota2 + nota3 + nota4) / 4
-print(f"A média final é: {media:.2f}")
+print(f"A sua média final é: {media:.2f}")
